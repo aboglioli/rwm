36bc728fcfbb414b10797fba0952e346495c328d
Alan Boglioli <alan.boglioli@gmail.com>
2020-05-03 04:44:48 -0300

    Use Iterator::enumerate to get index

--- src/core/layout.rs
+++ src/core/layout.rs
@@ -9,12 +9,11 @@ pub struct ColumnLayout(pub u32, pub u32);
 impl Layout for ColumnLayout {
     fn apply<'a>(&self, windows: Box<dyn Iterator<Item = &mut window::Window> + 'a>) {
         if let (_, Some(len)) = windows.size_hint() {
-            let mut i = 0;
             let (w, h) = (self.0 / len as u32, self.1);
-            for win in windows {
+            for (i, win) in windows.enumerate() {
+                let i = i as i32;
                 win.set_size(w, h);
                 win.set_position(i * w as i32, 0);
-                i += 1;
             }
         }
     }
@@ -25,12 +24,11 @@ pub struct RowLayout(pub u32, pub u32);
 impl Layout for RowLayout {
     fn apply<'a>(&self, windows: Box<dyn Iterator<Item = &mut window::Window> + 'a>) {
         if let (_, Some(len)) = windows.size_hint() {
-            let mut i = 0;
             let (w, h) = (self.0, self.1 / len as u32);
-            for win in windows {
+            for (i, win) in windows.enumerate() {
+                let i = i as i32;
                 win.set_size(w, h);
                 win.set_position(0, i * h as i32);
-                i += 1;
             }
         }
     }
